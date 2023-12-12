f3959b15e619242ebb45c3d621144ab1398d14cf
sharath <admin@Macbook-Air.local>
2023-12-12 21:24:10 +0530

    background image added

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ const App = () => {
     return (
         // if gradient applay this class ====  gradient-pink-orange
         // if Image applay this class's ==== bg-[url('https://www.baronmiedzyzdroje.pl/wp-content/uploads/2019/04/background-1.png')] bg-no-repeat bg-cover
-        <div className="w-full min-h-screen justify-center flex items-center gradient-orange-pink">
-            <div className="bg-white w-full h-screen md:w-[35vw] md:h-[60vh]">
+        // CHANGE THE IAMGE URL!
+
+        <div className="w-full min-h-screen justify-center flex items-center bg-[url('https://www.baronmiedzyzdroje.pl/wp-content/uploads/2019/04/background-1.png')] bg-no-repeat bg-cover">
+            <div className="bg-white w-full h-screen md:w-[35vw] md:h-[60vh] ">
                 <Header />
                 <Body />
             </div>
@@ -16,6 +18,3 @@ const App = () => {
 };
 
 export default App;
-
-// orange = "rgb(255, 130, 61, 1)"
-// purple = "rgb(248, 75, 126, 1)"
