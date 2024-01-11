0a3ff4813eaa1bde918623b764b74e33ccb6a8de
sharath <admin@Macbook-Air.local>
2024-01-11 09:07:54 +0530

    basic form validation added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,17 +1,112 @@
-import React from "react";
-import Input from "./Input";
+import React, { useState } from 'react';
+import Input from './Input';
 
 const Body = () => {
+    const [inputValue, setInputValue] = useState({
+        password: '',
+        username: '',
+    });
+
+    const [inputErrors, setInputErrors] = useState({
+        password: '',
+        username: '',
+    });
+
+    const validateUsername = () => {
+        const { username } = inputValue;
+        if (!username) {
+            setInputErrors((prevErrors) => ({
+                ...prevErrors,
+                username: 'Username is required',
+            }));
+            return false;
+        } else if (username.length < 4) {
+            setInputErrors((prevErrors) => ({
+                ...prevErrors,
+                username: 'Username must be at least 4 characters long',
+            }));
+            return false;
+        } else {
+            setInputErrors((prevErrors) => ({
+                ...prevErrors,
+                username: '',
+            }));
+            return true;
+        }
+    };
+
+    const validatePassword = () => {
+        const { password } = inputValue;
+        if (!password) {
+            setInputErrors((prevErrors) => ({
+                ...prevErrors,
+                password: 'Password is required',
+            }));
+            return false;
+        } else if (password.length < 6) {
+            setInputErrors((prevErrors) => ({
+                ...prevErrors,
+                password: 'Password must be at least 6 characters long',
+            }));
+            return false;
+        } else {
+            setInputErrors((prevErrors) => ({
+                ...prevErrors,
+                password: '',
+            }));
+            return true;
+        }
+    };
+
+    const { password, username } = inputValue;
+    const handleOnChange = (e) => {
+        const { name, value } = e.target;
+        setInputValue({
+            ...inputValue,
+            [name]: value,
+        });
+        setInputErrors({
+            ...inputErrors,
+            [name]: '', // Clear error when typing
+        });
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const isUsernameValid = validateUsername();
+        const isPasswordValid = validatePassword();
+
+        if (isUsernameValid && isPasswordValid) {
+            alert('Form submited successfully');
+        }
+    };
+
     return (
-        <div className="w-full h-[85%] flex flex-col justify-center items-start gap-6 p-10">
-            <Input text="Username" />
-            <Input text="Password" />
-            <div className="flex w-full px-8 gap-3">
-                <input className="accent-black" type="checkbox" />
-                <p className="font-medium">Remember Password</p>
+        <form onSubmit={handleSubmit} className='w-full h-[85%] flex flex-col justify-center items-start gap-6 p-10'>
+            <Input
+                type={'text'}
+                name={'username'}
+                value={username}
+                placeholder={'Enter Your Username'}
+                onChange={handleOnChange}
+            />
+            <p className='mt-0.5 visible text-sm text-red-600 dark:text-red-500'>{inputErrors.username}</p>
+            <Input
+                type={'password'}
+                name={'password'}
+                value={password}
+                placeholder={'Enter Your Password'}
+                onChange={handleOnChange}
+            />
+            <p className='mt-0.5 visible text-sm text-red-600 dark:text-red-500'>{inputErrors.password}</p>
+            <div className='flex w-full px-8 gap-3'>
+                <input className='accent-black' type='checkbox' />
+                <p className='font-medium'>Remember Password</p>
             </div>
-            <button className="gradient-orange-pink w-full text-white p-2 font-medium">Login</button>
-        </div>
+            <button type='submit' className='gradient-orange-pink w-full text-white p-2 font-medium'>
+                Login
+            </button>
+        </form>
     );
 };
 
--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React from 'react';
 
-const Input = ({ text }) => {
-    return <input className="px-3 py-2 w-full border-2 rounded bg-gray-50  outline-black" type="text" placeholder={text} />;
+const Input = ({ type, name, value, placeholder, onChange }) => {
+    return (
+        <input
+            className='px-3 py-2 w-full border-2 rounded bg-gray-50  outline-black'
+            type={type}
+            name={name}
+            value={value}
+            placeholder={placeholder}
+            onChange={onChange}
+        />
+    );
 };
 
 export default Input;
